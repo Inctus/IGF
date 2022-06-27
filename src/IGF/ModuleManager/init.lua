822c7643b20c96e2d01dab4da0d1fe9408408258
Haashim <git@haash.im>
2022-06-27 20:08:57 +0100

    Cleaned ModuleManager

--- src/IGF/ModuleManager/init.lua
+++ src/IGF/ModuleManager/init.lua
@@ -7,43 +7,49 @@ local Forest = require(script.Forest)
 local Node = require(script.Forest.Node)
 local Promise = require(script.Parent.Dependencies.Promise)
 
+--INFO: Handles all Module interactions
 local ModuleManager = {}
 ModuleManager.__index = ModuleManager
 
-function ModuleManager.new(IGF): ModuleManager
-    local self = setmetatable({}, ModuleManager)
+do
 
-    local inject = IGF.InjectionManager:GetInjector()
-    self.Forest = Forest.new(inject) :: Forest
+    function ModuleManager.new(IGF): ModuleManager
+        local self = setmetatable({}, ModuleManager)
 
-    return self
+        local inject = IGF.InjectionManager:GetInjector()
+        self.Forest = Forest.new(inject) :: Forest
+
+        return self
+    end
+
+    function ModuleManager:Add(is_shared: boolean, instance: Instance, eager_array: t.Array<ModuleScript>): Promise
+        return Promise.try(function()
+            self.Forest:AddTree(instance, eager_array, is_shared)
+        end)
+    end
+
+    function ModuleManager:Retrieve(from: ModuleScript, path: t.Array<string>, target_shared: boolean): Promise
+        return Promise.try(function()
+            self.Forest:Retrieve(from, path, target_shared):GetContent()
+        end)
+    end
+
+    function ModuleManager:Run(from: ModuleScript, path: t.Array<string>, target_shared: boolean, ...): Promise
+        local args = {...}
+        return Promise.try(function()
+            if #args > 0 then
+                self.Forest:Retrieve(from, path, target_shared):Run(table.unpack(args))
+            else
+                self.Forest:Retrieve(from, path, target_shared):Run()
+            end
+        end)
+    end
+
+    type ModuleManager = typeof(ModuleManager.new(print))
+    type Forest = typeof(Forest.new(print))
+
 end
 
-function ModuleManager:Add(is_shared: boolean, instance: Instance, eager_array: t.Array<ModuleScript>): Promise
-    return Promise.try(function()
-        self.Forest:AddTree(instance, eager_array, is_shared)
-    end)
-end
-
-function ModuleManager:Retrieve(from: ModuleScript, path: t.Array<string>, target_shared: boolean): Promise
-    return Promise.try(function()
-        self.Forest:Retrieve(from, path, target_shared):GetContent()
-    end)
-end
-
-function ModuleManager:Run(from: ModuleScript, path: t.Array<string>, target_shared: boolean, ...): Promise
-    local args = {...}
-    return Promise.try(function()
-        if #args > 0 then
-            self.Forest:Retrieve(from, path, target_shared):Run(table.unpack(args))
-        else
-            self.Forest:Retrieve(from, path, target_shared):Run()
-        end
-    end)
-end
-
-type ModuleManager = typeof(ModuleManager.new(print))
-type Forest = typeof(Forest.new(print))
 export type Promise = typeof(Promise.new())
 
 return ModuleManager
