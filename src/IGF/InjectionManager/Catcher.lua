1797e02686214bc459e2524b278b562a38b48e74
Haashim <git@haash.im>
2022-07-04 13:32:04 +0100

    Removed old copyright notice

--- src/IGF/InjectionManager/Catcher.lua
+++ src/IGF/InjectionManager/Catcher.lua
@@ -1,10 +1,4 @@
 --!strict
---[[    Catcher.lua | Copyright Notice
-
-        INC Game Framework - OS Game Framework for Roblox
-        Copyright (C) 2022 Inctus (Haashim-Ali Hussain)
-                
-        Full notice in IGF.lua      ]]--
 
 local Catcher = {}
 
